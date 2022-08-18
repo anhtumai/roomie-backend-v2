6bdf417f76d72b110ac5236020cbb16a2c7d620c
anhtumai <tu.mai@outlook.com>
2022-08-18 23:16:35 +0300

    add getMyProfileResolver

--- services/graphql-api/src/resolvers/Query.ts
+++ services/graphql-api/src/resolvers/Query.ts
@@ -1,10 +1,29 @@
 import { validateToken } from "./validateToken";
 
-async function getMyProfile(parent: any, args: any, context: any, info: any) {
-  const x = await validateToken(context.token);
-  console.log("JWT payload", x);
-  console.log("Context", context);
-  return {};
+import { connectMongodb } from "@roomie-backend-v2/models/database";
+import UserModel from "@roomie-backend-v2/models/user";
+
+connectMongodb();
+
+async function getMyProfileResolver(
+  parent: any,
+  args: any,
+  context: any,
+  info: any,
+) {
+  const jwtPayload = await validateToken(context.token);
+  if (typeof jwtPayload === "string") {
+    throw new Error("Invalid JWT Payload");
+  }
+  const user = await UserModel.findById(jwtPayload.sub);
+  if (user === null) {
+    throw new Error(`User with id ${jwtPayload.sub} not found`);
+  }
+  return {
+    id: user.id,
+    email: jwtPayload.email,
+    username: user.username,
+  };
 }
 
-export default { getMyProfile };
+export default { getMyProfile: getMyProfileResolver };
