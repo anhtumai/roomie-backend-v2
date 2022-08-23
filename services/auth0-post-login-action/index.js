e45c50db6297c2a74ca3b7635a3949377a60042d
anhtumai <tu.mai@outlook.com>
2022-08-23 18:39:04 +0300

    dont interrup login service if login flow malfunctions

--- services/auth0-post-login-action/index.js
+++ services/auth0-post-login-action/index.js
@@ -2,7 +2,9 @@ const axios = require("axios");
 
 exports.onExecutePostLogin = async (event, api) => {
   const url = event.secrets.USER_CREATION_HANDLER_URL || "";
-  const result = await axios.post(url, {
-    user: event.user,
-  });
+  try {
+    const result = await axios.post(url, {
+      user: event.user,
+    });
+  } catch (error) {}
 };
