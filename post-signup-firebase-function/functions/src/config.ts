28970164d4bc18d55d166d40f4417574ece769a8
anhtumai <tu.mai@outlook.com>
2023-03-04 13:18:05 +0200

    upgrade firebase-functions to version 4

--- post-signup-firebase-function/functions/src/config.ts
+++ post-signup-firebase-function/functions/src/config.ts
@@ -1,3 +1,5 @@
 export default {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb_uri_undefined",
+  MONGODB_URI:
+    process.env.MONGODB_URI ||
+    "mongodb://username:password@cluster0.random.mongodb.net",
 };
